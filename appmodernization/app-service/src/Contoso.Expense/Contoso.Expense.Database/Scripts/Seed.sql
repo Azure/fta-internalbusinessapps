b2d775ad724fcbe37430f11cd9617b2c81860ae1
prettyrita <47895932+prettyrita@users.noreply.github.com>
2023-08-08 15:59:27 -0700

    Update Seed.sql
    fixed FTE email addresses and names disclosed

--- appmodernization/app-service/src/Contoso.Expense/Contoso.Expense.Database/Scripts/Seed.sql
+++ appmodernization/app-service/src/Contoso.Expense/Contoso.Expense.Database/Scripts/Seed.sql
@@ -2,8 +2,8 @@
 
 BEGIN
 
-INSERT INTO dbo.Expense (Purpose, ExpenseDate, Amount, EmployeeName, EmployeeEmail, ApproverName, ApproverEmail, Receipt)  values ('FastTrack team visit-1', '2017-7-10', 1500, 'Umar Mohamed', 'umarm@microsoft.com', 'Dennis Karlinsky', 'denniska@microsoft.com', 'http://contosocandies-cdnep.azureedge.net/cdn/candie-3.jpg')
-INSERT INTO dbo.Expense (Purpose, ExpenseDate, Amount, EmployeeName, EmployeeEmail, ApproverName, ApproverEmail, Receipt)  values ('FastTrack team visit-2', '2017-6-11', 990, 'Faisal Mustafa', 'faisalm@microsoft.com', 'Dennis Karlinsky', 'denniska@microsoft.com', 'http://contosocandies-cdnep.azureedge.net/cdn/candie-3.jpg')
-INSERT INTO dbo.Expense (Purpose, ExpenseDate, Amount, EmployeeName, EmployeeEmail, ApproverName, ApproverEmail, Receipt)  values ('FastTrack team visit-3', '2017-5-12', 1200, 'Randy Pagels', 'umarm@microsoft.com', 'Anusha Rangaswamy', 'anushar@microsoft.com', 'http://contosocandies-cdnep.azureedge.net/cdn/candie-3.jpg')
+INSERT INTO dbo.Expense (Purpose, ExpenseDate, Amount, EmployeeName, EmployeeEmail, ApproverName, ApproverEmail, Receipt)  values ('FastTrack team visit-1', '2017-7-10', 1500, 'Fakeuser One', 'fakeuser@contoso.com', 'Fakeuser One', 'fakeuser@contoso.com', 'http://contosocandies-cdnep.azureedge.net/cdn/candie-3.jpg')
+INSERT INTO dbo.Expense (Purpose, ExpenseDate, Amount, EmployeeName, EmployeeEmail, ApproverName, ApproverEmail, Receipt)  values ('FastTrack team visit-2', '2017-6-11', 990, 'Fakeuser Two', 'fakeuser2@contoso.com', 'Fakeuser Two', 'fakeuser2@contoso.com', 'http://contosocandies-cdnep.azureedge.net/cdn/candie-3.jpg')
+INSERT INTO dbo.Expense (Purpose, ExpenseDate, Amount, EmployeeName, EmployeeEmail, ApproverName, ApproverEmail, Receipt)  values ('FastTrack team visit-3', '2017-5-12', 1200, 'Fakeuser Three', 'fakeuser3@contoso.com', 'Fakeuser Three', 'fakeuser3@contoso.com', 'http://contosocandies-cdnep.azureedge.net/cdn/candie-3.jpg')
 
 END
