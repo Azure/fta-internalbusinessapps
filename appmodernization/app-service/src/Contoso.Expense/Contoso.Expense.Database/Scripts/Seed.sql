973f7bc0693125d59d43258529ff31466f907a6c
Jelle Druyts <jelle.druyts@microsoft.com>
2024-11-22 09:05:51 +0100

    Update Seed.sql

--- appmodernization/app-service/src/Contoso.Expense/Contoso.Expense.Database/Scripts/Seed.sql
+++ appmodernization/app-service/src/Contoso.Expense/Contoso.Expense.Database/Scripts/Seed.sql
@@ -2,8 +2,8 @@
 
 BEGIN
 
-INSERT INTO dbo.Expense (Purpose, ExpenseDate, Amount, EmployeeName, EmployeeEmail, ApproverName, ApproverEmail, Receipt)  values ('FastTrack team visit-1', '2017-7-10', 1500, 'Umar Mohamed', 'umarm@microsoft.com', 'Dennis Karlinsky', 'denniska@microsoft.com', 'http://contosocandies-cdnep.azureedge.net/cdn/candie-3.jpg')
-INSERT INTO dbo.Expense (Purpose, ExpenseDate, Amount, EmployeeName, EmployeeEmail, ApproverName, ApproverEmail, Receipt)  values ('FastTrack team visit-2', '2017-6-11', 990, 'Faisal Mustafa', 'faisalm@microsoft.com', 'Dennis Karlinsky', 'denniska@microsoft.com', 'http://contosocandies-cdnep.azureedge.net/cdn/candie-3.jpg')
-INSERT INTO dbo.Expense (Purpose, ExpenseDate, Amount, EmployeeName, EmployeeEmail, ApproverName, ApproverEmail, Receipt)  values ('FastTrack team visit-3', '2017-5-12', 1200, 'Randy Pagels', 'umarm@microsoft.com', 'Anusha Rangaswamy', 'anushar@microsoft.com', 'http://contosocandies-cdnep.azureedge.net/cdn/candie-3.jpg')
+INSERT INTO dbo.Expense (Purpose, ExpenseDate, Amount, EmployeeName, EmployeeEmail, ApproverName, ApproverEmail, Receipt)  values ('FastTrack team visit-1', '2017-7-10', 1500, 'Umar', 'u@example.org', 'Dennis', 'd@example.org', 'http://contosocandies-cdnep.azureedge.net/cdn/candie-3.jpg')
+INSERT INTO dbo.Expense (Purpose, ExpenseDate, Amount, EmployeeName, EmployeeEmail, ApproverName, ApproverEmail, Receipt)  values ('FastTrack team visit-2', '2017-6-11', 990, 'Faisal', 'f@example.org', 'Dennis', 'd@example.org', 'http://contosocandies-cdnep.azureedge.net/cdn/candie-3.jpg')
+INSERT INTO dbo.Expense (Purpose, ExpenseDate, Amount, EmployeeName, EmployeeEmail, ApproverName, ApproverEmail, Receipt)  values ('FastTrack team visit-3', '2017-5-12', 1200, 'Randy', 'u@example.org', 'Anusha', 'a@example.org', 'http://contosocandies-cdnep.azureedge.net/cdn/candie-3.jpg')
 
 END
